5b17bc281edeb44caa839aac69b4a3baf1006ef5
Mehmet Karaahmetoglu <mehmetkaraahmetoglu@Mehmet-MBP.fritz.box>
2023-10-25 18:02:20 +0200

    Pom.xml is updated_Mehmet

--- src/test/java/tests/m_emin/US_013.java
+++ src/test/java/tests/m_emin/US_013.java
@@ -3,5 +3,7 @@ package tests.m_emin;
 public class C01 {
     public static void main(String[] args) {
         System.out.println("hayirli olsun");
+   // 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
+
     }
 }
