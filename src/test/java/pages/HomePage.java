e541c09dffe92b05dd107e1eab0c547e1b405973
Mehmet Karaahmetoglu <mehmetkaraahmetoglu@Mehmet-MBP.fritz.box>
2023-11-01 11:53:59 +0100

    Admin dashboard

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -12,15 +12,96 @@ public class HomePage {
 
    // Header/Features Link
     @FindBy(xpath="//a[text()='Features']")
-    public WebElement FeaturesTextLink;
+    public WebElement FeaturesTextLinkME;
 
     //Homepage SignIn Button
     @FindBy(xpath = "//a[@class='btn btn-white fs-18 ms-3 d-lg-block d-none']")
-    public WebElement signInButton;
+    public WebElement signInButtonME;
 
-    @FindBy(xpath = "sonol locate");
-    @FindBy(xpath = "sonol locate");
-    @FindBy(xpath = "sonol locate");
+    @FindBy(xpath = "//input[@name='email']")
+    public WebElement logInEMailME;
+
+    @FindBy(xpath="//input[@name='password']")
+    public WebElement logInPasswordME;
+
+    @FindBy(xpath = "//button[@class='btn btn-primary']")
+    public WebElement loginButtonME;
+
+
+
+    ///////Bilal
+ @FindBy(xpath="//a[text()='Features']")
+ public WebElement FeaturesTextLink;
+ //Homepage SignIn Button
+ @FindBy(xpath = "//a[@class='btn btn-white fs-18 ms-3 d-lg-block d-none']")
+ public WebElement signInButton;
+ // funcktions footer
+ @FindBy(xpath = "//*[@target='_blank']")
+ public WebElement footerbd;
+ @FindBy(xpath = "//a[@title='Facebook']")
+ public WebElement footerFacebookbd;
+ @FindBy(xpath = "(//button[@type=\"submit\"])[3]")
+ public WebElement facebookCookiesbd;
+ @FindBy(xpath = "//div[@class=\"_8esk\"]")
+ public WebElement facebookLoginSayfasibd;
+ @FindBy(xpath = "//a[@title='Twitter']")
+ public WebElement footerXbd;
+ @FindBy(xpath = "//div[@id=\"react-root\"]")
+ public WebElement xLoginSayfasibd;
+ @FindBy(xpath = "//a[@title='Instagram']")
+ public WebElement footerInstagrambd;
+ @FindBy(xpath = "//main[@class=\"_a993 _a994\"]")
+ public WebElement instagramLoginSayfabd;
+ @FindBy(xpath = "//button[text()='Allow all cookies']")
+ public WebElement instagramCookiesbd;
+ @FindBy(xpath = "//a[@title='Linkedin']")
+ public WebElement footerLinkedinbd;
+ @FindBy(xpath = "(//button[@type=\"submit\"])[4]")
+ public WebElement linkedinbd;
+ @FindBy(xpath = "//a[@title='Pinterest']")
+ public WebElement footerPinterestbd;
+ @FindBy(xpath = "//div[@id='__PWS_ROOT__']")
+ public WebElement pnterestHomePagebd;
+ @FindBy(xpath = "(//a[@target=\"_blank\"])[6]")
+ public WebElement footerTermsConditionsbd;
+ @FindBy(xpath = "(//a[@target=\"_blank\"])[7]")
+ public WebElement footePprivacyPolicybd;
+ @FindBy(xpath = "//section[@class=\"hero-section\"]")
+ public WebElement privacyPolicybd;
+ @FindBy(xpath = "(//a[@target=\"_blank\"])[8]")
+ public WebElement footerFaqbd;
+ @FindBy(xpath = "(//div[@class=\"container\"])[1]")
+ public WebElement faqbd;
+ @FindBy(xpath = "(//a[@data-turbo=\"false\"])[1]")
+ public  WebElement homePageSignInButonubd;
+ @FindBy(xpath = "//a[@class='btn btn-white fs-18 ms-3 d-lg-block d-none']")
+ public WebElement getSignInButtonbd;
+ @FindBy(id = "email")
+ public WebElement emailbd;
+ @FindBy(id = "password")
+ public WebElement passwordbd;
+ @FindBy(xpath = "//button[@type='submit']")
+ public WebElement loginbd;
+ @FindBy(xpath = "//button[@id='dropdownMenuButton1']")
+ public WebElement testTeambd;
+ @FindBy(xpath = "(//a[@class='dropdown-item text-gray-900'])[1]")
+ public WebElement accountSettingsbd;
+
+
+ //// senol
+
+ @FindBy(id = "email")
+ public WebElement signInEmailElementiSM;
+ @FindBy(id = "password")
+ public WebElement signInPasswordElementiSM;
+ @FindBy(xpath = "//button[@type='submit']")
+ public WebElement LoginButtonuSM;
+
+
+
+
+
+
 
 
 
--- src/test/java/pages/UserDashboard.java
+++ src/test/java/pages/UserDashboard.java
@@ -1,4 +1,122 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class UserDashboard {
+    public UserDashboard(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy(xpath = "//input[@name='email']")
+    public WebElement logInEMailME;
+
+    @FindBy(xpath="//input[@name='password']")
+    public WebElement logInPasswordME;
+
+    @FindBy(xpath = "//button[@class='btn btn-primary']")
+    public WebElement loginButtonME;
+
+    @FindBy(xpath = "//button[@class='btn dropdown-toggle ps-2 pe-0']")
+    public WebElement userMenuME;
+
+
+    @FindBy(xpath = "(//a[@class='dropdown-item text-gray-900'])[1]")
+    public WebElement accountSettingsME;
+
+    @FindBy(xpath = "//*[@class='svg-inline--fa fa-pen']")
+    public WebElement profilResmiDuzenleIkonuME;
+
+
+    @FindBy(xpath = "//a[@class='nav-link p-0 active']")
+    public WebElement profileDetailsME;
+
+    @FindBy(xpath = "//input[@id='editProfileFirstName']")
+    public WebElement userSettingsFirstNameME;
+
+    @FindBy(xpath = "//input[@id='editProfileLastName']")
+    public WebElement userSettingsLastNameME;
+
+    @FindBy(xpath = "//input[@id='isEmailEditProfile']")
+    public WebElement userSettingsEmailME;
+
+    @FindBy(xpath = "//div[@class='iti__selected-flag']")
+    public WebElement userSettingsNumberFlagsME;
+
+    @FindBy(xpath = "//*[@class='iti__country iti__standard']")
+    public WebElement bayrak_MenusuME;
+
+    @FindBy(xpath = "(//*[@class='iti__country iti__standard'])[5]")
+    public WebElement andorraFlagME;
+
+    @FindBy(xpath = "//input[@value='Save']")
+    public WebElement saveButtonME;
+
+    @FindBy(xpath = "//input[@placeholder='Phone Number']")
+    public WebElement userSettingsPhoneME;
+
+
+
+    ///// senol
+
+    @FindBy(xpath = "(//*[@class='nav-link d-flex align-items-center py-3'])[2]")
+    public WebElement vCardsElementiSM;
+    @FindBy(xpath = "//*[@class='d-flex justify-content-end']")
+    public WebElement newCardElementiSM;
+    @FindBy(xpath = "//*[@class='form-control ms-1 vcard-url-alias']")
+    public WebElement urlAliasElementiSM;
+    @FindBy(xpath = "//*[@placeholder='Enter VCard Name']")
+    public WebElement vCardNameSM;
+    @FindBy(xpath = "//*[@placeholder='Enter Occupation']")
+    public WebElement occupationElementSM;
+    @FindBy(xpath = "//*[@class='ql-editor ql-blank']")
+    public WebElement descriptionElementSM;
+    @FindBy(xpath = "//input[@id='vcardSaveBtn']")
+    public WebElement newVCardSaveButonuSM;
+    @FindBy (xpath = "//*[@*='generate-url-alias']")
+    public WebElement urlAliasSM;
+    @FindBy(xpath = "//*[@placeholder='Enter First Name']")
+    public WebElement vCardFirstNameSM;
+    @FindBy(xpath = "//*[@placeholder='Enter Last Name']")
+    public WebElement vCardLastNameSM;
+    @FindBy(xpath = "//*[@placeholder='Enter Email Address']")
+    public WebElement vCardEmailSM;
+    @FindBy(xpath = "//*[@placeholder='Alternate Email']")
+    public WebElement vCardAlternateEmailSM;
+    @FindBy(xpath = "//*[@placeholder='Enter Phone Number']")
+    public WebElement vCardPhoneSM;
+    @FindBy(xpath = "//*[@placeholder='Alternate Phone']")
+    public WebElement vCardAlternatePhoneSM;
+    @FindBy(xpath = "//*[@placeholder='Enter Your Location']")
+    public WebElement vCardLocationSM;
+    @FindBy(xpath = "//*[@placeholder='Enter Your Location URL']")
+    public WebElement vCardLocationUrlSM;
+    @FindBy(xpath = "//*[@placeholder='Enter Date of Birth']")
+    public WebElement vCardDateOfBirthSM;
+    @FindBy(xpath = "//*[@placeholder='Enter Company Name']")
+    public WebElement vCardCompanySM;
+    @FindBy(xpath = "//*[@placeholder='Enter Job Title']")
+    public WebElement vCardJobTitleSM;
+    @FindBy(xpath = "//span[@id='select2-default_language-container']")
+    public WebElement vCardJLanguageSM;
+    @FindBy(xpath = "//input[@type='submit']")
+    public WebElement vCardSaveButonuSM;
+    @FindBy(xpath = "//*[@id='select2-default_language-result-a2mc-en']")
+    public WebElement vCardEnglishSM;
+    @FindBy(xpath = "(//*[@class='text-decoration-none fs-6'])[1]")
+    public WebElement previewUrlSM;
+    @FindBy(xpath = "(//a[@class='btn px-1 text-primary fs-3'])[1]")
+    public WebElement vCardEditSM;
+    @FindBy (xpath = "//a[@class='btn btn-outline-primary float-end']")
+    public WebElement backButtonSM;
+    @FindBy(xpath = "//span[text()='Engineer']")
+    public WebElement editMeslekElementiSM;
+    @FindBy(xpath = "//*[@class='vcard1-sticky-btn bars-btn d-flex justify-content-center text-white me-5 align-items-center rounded-0 px-5 mb-3 text-decoration-none py-1 rounded-pill justify-content-center']")
+    public WebElement kartfonksiyonaktifElementiSM;
+    @FindBy(xpath = "//*[@data-icon='share-nodes']")
+    public WebElement qRKodeSosyalMedyaLinkiSM;
+    @FindBy(xpath = "//span[@class='svg-icon svg-icon-1']")
+    public WebElement cikisKapatmaSM;
+
 }
